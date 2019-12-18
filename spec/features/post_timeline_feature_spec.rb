088e69ed8cb86da29e6e75cc561fe30045073b2f
Anh Nguyen <anhnguyenis@gmail.com>
2019-12-18 15:54:53 +0000

    Add feature test for post timeline

--- spec/features/post_timeline_feature_spec.rb
+++ spec/features/post_timeline_feature_spec.rb
@@ -3,26 +3,27 @@
 require 'rails_helper'
 require_relative './login_helper'
 
-RSpec.feature 'Timeline', type: :feature do
-  scenario 'Can submit posts and view them' do
-    sign_up_user
+RSpec.feature 'Post timeline feature', type: :feature do
+  before(:each) do
+    log_in_user
     visit '/posts'
-    click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
-    click_button 'Submit'
+  end
+
+  scenario 'Can submit posts and view them' do
     expect(page).to have_content('Hello, world!')
     expect(find_all('.card-subtitle').first.text).to match(/\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\w{3}/)
   end
 
   scenario 'Can submit multiple posts and view them in reverse chronological order' do
-    sign_up_user
-    visit '/posts'
-    click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
-    click_button 'Submit'
     click_link 'New post'
     fill_in 'Message', with: 'Hello, universe!'
     click_button 'Submit'
     expect(find('.container').text).to match(/Hello, universe!.*Hello, world!/)
+  end
+
+  scenario 'User can delete posts' do
+    expect(find('.container').text).to match(/Hello, world!/)
+    find_all('.button-delete').first.click
+    expect(find('.container').text).not_to match(/Hello, world!/)
   end
 end
