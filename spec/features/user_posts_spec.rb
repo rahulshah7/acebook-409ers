19f660f7c814c6aad3732863583ac6cfde25966e
Sam King <samuelking94@gmail.com>
2019-12-19 12:04:25 +0000

    Implement My Posts page
    Conditional in navbar for link to current_user.id or root to prevent error
    
    Login helper method for alt user
    
    Feature test to assert that My Posts page contains only current users posts
    
    BUG found with simple_format rails method. <p> tags come out of sync, can't get a handle on element

---
+++ spec/features/user_posts_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative './login_helper'
+
+RSpec.feature 'Post timeline feature', type: :feature do
+  before(:each) do
+    log_in_user
+    visit '/posts'
+  end
+
+  scenario 'Can submit posts and view them' do
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, universe!'
+    click_button "Submit"
+    click_link 'Log Out'
+    log_in_alt_user
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button "Submit"
+    click_link 'My Posts'
+    expect(page).not_to have_content(test_users(0)[:email].split('@').first)
+    expect(find_all('.card-title').first.text).to have_content(test_users(1)[:email].split('@').first)
+  end
+end
