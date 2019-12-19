21aa8c1521be8248e985d053f0de025ff982f92b
Rahul Shah <rahulshah7@outlook.com>
2019-12-19 11:53:10 +0000

    User can edit any post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,30 @@
 
 class PostsController < ApplicationController
   def new
+    @form_title = 'New Post'
     @post = Post.new
+  end
+
+  def edit
+    @form_title = 'Edit Post'
+    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.create(message: post_params[:message], user_id: current_user.id)
+    flash[:notice] = 'Successfully posted.'
     redirect_to posts_url
   end
 
   def index
     @posts = Post.all.reverse
+  end
+
+  def update
+    @post = Post.find(params['id'])
+    @post.update(message: post_params[:message])
+    flash[:notice] = 'Successfully updated post.'
+    redirect_to posts_url
   end
 
   def destroy
@@ -20,7 +34,7 @@ class PostsController < ApplicationController
 
     if user_id == current_user.id
       @post = Post.destroy(post_id)
-      flash[:notice] = "Successfully deleted."
+      flash[:notice] = 'Successfully deleted.'
     else
       flash[:alert] = "Cannot delete another user's post."
     end
